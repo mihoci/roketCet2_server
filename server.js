28e09d319d058df738ca361edea1201a2c4cd33c
Mihoci <mihoci.patrik166@gmail.com>
2019-02-27 09:52:24 +0100

    to redis

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
 const WebSocket = require('ws');
 const express = require('express')
 const http = require('http')
+const redis = require('redis')
 
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT
+
+let redisClient = redis.createClient();
+redisClient.on('connect',()=>{
+  console.log('connected to redis')
+})
 
 app.get('/', (req, res)=>{
   res.send(port)
 })
 
 server.listen(port || 5001)
-
 wss = new WebSocket.Server({server: server})
 
+wss.on('connection', (ws, req) => {   
+      ws.on('message', (message) => {
+        redisClient.lpush('messages', message)
+        for(const client of wss.clients){
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
+        }
+      })
 
-const messages = []
+      ws.on('close',() =>  {
+          console.log('client disconnected')
+      })
 
-wss.on('connection', (ws, req) => {
-
-    ws.on('message', (message) => {
-        messages.unshift(JSON.parse(message));
-        console.log(messages)
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(message);
-            }
-          });
-    })
-
-    ws.on('close', () =>  {
-        console.log('client disconnected')
-    })
-
-    
-    ws.send(JSON.stringify(messages))
+      //send all messages to client
+      redisClient.lrange('messages', 0, -1, (err, reply)=>{
+        if(err){
+          return err;
+        }
+        console.log(reply)
+        ws.send(JSON.stringify(reply));
+      })
     console.log('client connected')
 })
